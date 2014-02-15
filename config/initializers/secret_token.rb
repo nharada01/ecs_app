75f883b76e5b95f82147c4df5692492f3025e170
CentOS-6.3-x86_64 <harada@localhost.localdomain>
2014-02-15 22:59:39 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcsApp::Application.config.secret_token = '5d435516b1508720a9b88fc7d45fab5a5be60b58e2205b4a941a1db2fc4ed28f7b6d8d02a74b68607937bed166e2f25e08e3da4d8d7ac8b93e09a3c79acb500a'
